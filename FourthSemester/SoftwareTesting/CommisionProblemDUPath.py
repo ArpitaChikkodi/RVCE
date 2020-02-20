860d4eb858561f890fc24ace4fd41ab0992cd684
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-02-20 07:15:26 +0530

    init
    simplified

--- FourthSemester/SoftwareTesting/CommisionProblemDUPath.py
+++ FourthSemester/SoftwareTesting/CommisionProblemDUPath.py
@@ -1,13 +1,8 @@
-def testCom():
+def testCommission():
     global locks, stocks, barrels, totalSales, commission
     global lsales, ssales, bsales
-    lockPrice = 45
-    stockPrice = 30
-    barrelPrice = 25
-    count = 0
-    totalLock = 0
-    totalStocks = 0
-    totalBarells = 0
+    lockPrice,stockPrice,barrelPrice = 45,30,25
+    count,totalLock,totalStocks,totalBarrels = 0,0,0,0
     locks = int(input("Enter the total number of locks : "))
     while locks != -1:
         stocks = int(input("Enter the total number of stocks : "))
@@ -19,36 +14,35 @@ def testCom():
         else:
             totalLock += locks
             totalStocks += stocks
-            totalBarells += barrels
+            totalBarrels += barrels
             locks = int(
                 input("Enter the number of locks and to exit the loop enter -1 for locks :- "))
 
     print("total locks :- ", totalLock)
     print("total stocks :- ", totalStocks)
-    print("total barrel :- ", totalBarells)
+    print("total barrel :- ", totalBarrels)
     lsales = (totalLock * lockPrice)
     ssales = (totalStocks * stockPrice)
-    bsales = (totalBarells * barrelPrice)
+    bsales = (totalBarrels * barrelPrice)
     totalSales = lsales + ssales + bsales
     print("Total sales is : ", totalSales)
     if totalSales in range(1001, 1801):
         commission = 0.10 * 1000
         commission = commission + (0.15 * (totalSales - 1000))
     elif totalSales > 1800:
-        commission = 0.10 * 1000  # for less than 1000
-        commission = commission + (0.15 * 800)  # for range in 1000,1800
-        commission = commission + (0.20 * (totalSales - 1800))  # above 1800
+        commission = 0.10 * 1000  # for less than 1000 totalsales
+        commission = commission + (0.15 * 800)  # for sales range in 1000 to 1800
+        commission = commission + (0.20 * (totalSales - 1800))  # totalsales above 1800
     else:
         commission = 0.10 * totalSales
     print("Commission is : ", commission)
 
 
-testCom()
+testCommission()
 while True:
-    print("Want to retest yes(1) or no(0)")
-    ch = int(input("Enter 1 or 0 : "))
+    ch = int(input("Enter 1 to retest or 0 to exit : "))
     if ch == 1:
-        testCom()
+        testCommission()
     if ch == 0:
-        print("Testing done!!Leaving")
+        print("Testing done!")
         exit()
