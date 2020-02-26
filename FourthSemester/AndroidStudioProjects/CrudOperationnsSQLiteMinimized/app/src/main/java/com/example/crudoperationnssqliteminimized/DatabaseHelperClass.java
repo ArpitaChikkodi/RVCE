1e072894a522de202da632deb421f4a31f4d7747
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-02-26 08:16:32 +0530

    initial

---
+++ FourthSemester/AndroidStudioProjects/CrudOperationnsSQLiteMinimized/app/src/main/java/com/example/crudoperationnssqliteminimized/DatabaseHelperClass.java
@@ -0,0 +1,66 @@
+package com.example.crudoperationnssqliteminimized;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelperClass extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "automobile.db";
+    public static final String TABLE_NAME = "automobile_table";
+    public static final String AUTO_ID = "id";
+    public static final String AUTO_NAME = "name";
+    public static final String AUTO_YEAR = "year";
+
+    public DatabaseHelperClass(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (id varchar(30) primary key, name text, year text) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String id, String name, String year ){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AUTO_ID,id);
+        contentValues.put(AUTO_NAME,name);
+        contentValues.put(AUTO_YEAR,year);
+        long result = sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+        return cursor;
+    }
+
+    public boolean updateDataThroughId(String id, String name, String year){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AUTO_ID,id);
+        contentValues.put(AUTO_NAME,name);
+        contentValues.put(AUTO_YEAR,year);
+        sqLiteDatabase.update(TABLE_NAME,contentValues, " id = ? ", new String[] { id });
+        return true;
+    }
+
+    public Integer deleteDataThroughId(String id){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        return sqLiteDatabase.delete(TABLE_NAME,"id = ? ",new String[] { id });
+    }
+}
---
+++ FourthSemester/AndroidStudioProjects/CrudOperationnsSQLiteMinimized/app/src/main/java/com/example/crudoperationnssqliteminimized/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.crudoperationnssqliteminimized;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelperClass databaseHelperClass;
+    EditText id,name,year;
+    Button createTbl,addData,updateData,deleteData,viewData;
+    ImageButton emailData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseHelperClass = new DatabaseHelperClass(this);
+        id = findViewById(R.id.editId);
+        name = findViewById(R.id.editName);
+        year = findViewById(R.id.editYear);
+
+        //createTbl = findViewById(R.id.btnCreate);
+        addData = findViewById(R.id.btnAdd);
+        viewData = findViewById(R.id.btnView);
+        updateData = findViewById(R.id.btnUpdate);
+        deleteData = findViewById(R.id.btnDelete);
+        emailData = findViewById(R.id.btnMail);
+
+
+        AddData();
+        UpdateData();
+        ViewAll();
+        DeleteData();
+        SendToMail();
+//        createTable();
+    }
+
+//    public void createTable(){
+//        createTbl.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                boolean isCreated = databaseHelperClass.onCreate();
+//            }
+//        });
+//    }
+
+
+    public void AddData(){
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = databaseHelperClass.insertData(id.getText().toString(), name.getText().toString(), year.getText().toString());
+                if (isInserted == true) {
+                    Toast.makeText(MainActivity.this,"Data Inserted !!!",Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Data not Inserted !!!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void UpdateData(){
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isUpdated = databaseHelperClass.updateDataThroughId(id.getText().toString(),name.getText().toString(),year.getText().toString());
+
+                if(isUpdated == true){
+                    Toast.makeText(MainActivity.this,"Data Updated !!!",Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Data not updated !!!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void DeleteData(){
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer deletedRows = databaseHelperClass.deleteDataThroughId(id.getText().toString());
+                if( deletedRows > 0){
+                    Toast.makeText(MainActivity.this,"Data deleted !!!",Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Data not deleted !!!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void ViewAll(){
+        viewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = databaseHelperClass.getAllData();
+                if(cursor.getCount() == 0){
+                    showMessage("Error", "No record found ");
+                    Toast.makeText(MainActivity.this,"Data not available !!!",Toast.LENGTH_LONG).show();
+                    return;
+                }else {
+                    StringBuffer stringBuffer = new StringBuffer();
+                    while (cursor.moveToNext()){
+                        stringBuffer.append("ID : " + cursor.getString(0)+"\n");
+                        stringBuffer.append("NAME : " + cursor.getString(1)+"\n");
+                        stringBuffer.append("YEAR : " + cursor.getString(2)+"\n\n");
+//                        stringBuffer.append("---------------------------------------");
+                    }
+                    showMessage("DATA : ",stringBuffer.toString());
+                }
+            }
+        });
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    public void SendToMail(){
+        emailData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sid = id.getText().toString();
+                String nm = name.getText().toString();
+                String mark = year.getText().toString();
+                Intent intentMail = new Intent(Intent.ACTION_SEND);
+                intentMail.putExtra(Intent.EXTRA_EMAIL, new String[]{"apschikkodi@gmail.com"});
+                intentMail.putExtra(Intent.EXTRA_SUBJECT, "Sending mail from Android App");
+                intentMail.putExtra(Intent.EXTRA_TEXT,"Id : " + sid +"\nNAME : " + nm + "\nYear : " + mark);
+                intentMail.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(intentMail,"Choose Mail App"));
+            }
+        });
+    }
+}
